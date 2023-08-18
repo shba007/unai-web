934ca67845463194dcae08cc9ece65c2d07c7f27
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-08-18 23:14:17 +0530

    root url fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ai-guide.com',
+  site: 'https://shba007.github.io/ai-guide',
   integrations: [tailwind(), mdx(), sitemap()]
 });
