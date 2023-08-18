910645448b4c72b888b26eef7f440b6b31009c9a
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-08-19 00:37:26 +0530

    removed ai-guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shba007.github.io',
-  base: '/ai-guide',
+  site: 'https://ai-guide.onrender.com',
   integrations: [tailwind(), mdx(), sitemap()]
 });
